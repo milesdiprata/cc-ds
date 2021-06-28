e4c1583126c06a56d9ccef17e8879d9e4bb26df2
Miles di Prata <milesdiprata3@gmail.com>
2021-06-28 19:54:42 -0400

    stack: Cleaned up main file

--- src/milesdiprata/main.cc
+++ src/milesdiprata/main.cc
@@ -1,46 +1,10 @@
 #include <iostream>
 
-#include "milesdiprata/datastructure/stack/dynamic_range_stack.h"
-
 int main(const int argc, const char* const argv[]) {
   auto words = {"Hello, ", "world!", "\n"};
   for (const auto& word : words) {
     std::cout << word;
   }
-
-  auto s = milesdiprata::datastructure::DynamicRangeStack<int>();
-
-  for (int i = 0; i < 10; ++i) {
-    s.Push(i + 1);
-  }
-  std::cout << s.Maximum() << "\n";
-  std::cout << s.Minimum() << "\n";
-  std::cout << s << "\n";
-
-  s.Push(11);
-
-  std::cout << s.Maximum() << "\n";
-  std::cout << s.Minimum() << "\n";
-  std::cout << s << "\n";
-
-  s.Pop();
-  std::cout << s.Maximum() << "\n";
-  std::cout << s.Minimum() << "\n";
-  std::cout << s << "\n";
-
-  s.Pop();
-  std::cout << s.Maximum() << "\n";
-  std::cout << s.Minimum() << "\n";
-  std::cout << s << "\n";
-
-  std::cout << s << "\n";
-
-  s.Clear();
-
-  std::cout << s << "\n";
-
-  s.Pop();
-  std::cout << s << "\n";
 
   return 0;
 }
